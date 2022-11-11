481deb58455707f49bfe0855e998569f40aa4878
daiterranova <daiana.terranova@gmail.com>
2022-11-11 14:32:11 -0300

    revert commit Revert "add changes"
    This reverts commit 0bbc24d958651f8924959f87a13dc0a7b880df76.

--- myTerrarium/interaction/clones.js
+++ myTerrarium/interaction/clones.js
@@ -1,51 +1,14 @@
-const button = document.querySelector(".clone-btn");
-const divPlant = document.getElementsByClassName("plant__holder");
-const leftContainer = document.querySelector("#left");
-
-let selected = false;
-let plantSource;
-button.addEventListener("click", cloningPlant());
-
-/* changing the class of the plant when selected, and taking the html for later */
-/* ver OB del 7 al 11 y reestructurar codigo */
-
-function setPlanSource(src) {
-  return (this.plantSource = src);
-}
-
-plants.forEach((plant) => {
-  plant.addEventListener("click", (e) => {
-    plant.classList.remove("plant-hover");
-    plant.classList.add("selected");
-    console.log(typeof plant.src);
-    setplantSource(plant.src);
-    selected = true;
-  });
-});
-console.log(plantSource);
-/* template function to create html elements */
-
-function elementFromHtml(html) {
-  const template = document.createElement("template");
-  template.innerHTML = html.trim();
-  return template.content.firstElementChild;
-}
-
-function removeClasses() {
-  plants.forEach((plant) => {
-    plant.classList.remove("selected");
-    /*  plant.removeEventListener("click", function () {}); */
-  });
-}
+button.addEventListener("click", cloningPlant);
+const firstPlantLeft = document.getElementById("plant1");
 
 function cloningPlant() {
-  if (selected == false) {
-    return;
-  }
-  let imgClone = document.createElement("img");
-  imgClone.src = plantSource;
-  imgClone.setAttribute("class", "plant");
-  leftContainer.appendChild(imgClone);
-  dragElement(imgClone);
-  removeClasses();
+  /* una nueva planta 
+    crear una img 
+    ponerla dentro del div de la primera planta => check
+    ponerla dentro de cada div de cada planta */
+  const clonedPlant = elementFromHtml(`
+    <img class='plant' src='../images/plant1.png' alt='newplant'>
+    `);
+  firstPlantLeft.appendChild(clonedPlant);
+  dragElement(clonedPlant);
 }
